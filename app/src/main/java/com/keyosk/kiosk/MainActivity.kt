caf599473b18ecea0edd1f1d3f98773b13ab2458
leeyoonjie <ratte00@naver.com>
2023-07-19 11:59:10 +0900

    Main/splash/second

--- app/src/main/java/com/keyosk/kiosk/MainActivity.kt
+++ app/src/main/java/com/keyosk/kiosk/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.keyosk.kiosk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val moveButton = findViewById<Button>(R.id.moveButton)
+
+        // 페이지 이동
+        fun moveToAnotherPage(){
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+        // 함수호출
+        moveButton.setOnClickListener{
+            moveToAnotherPage()
+        }
     }
 }
