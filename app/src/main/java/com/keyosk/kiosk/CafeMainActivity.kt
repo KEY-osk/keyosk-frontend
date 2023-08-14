994ef5be841d1df4d5ec1af51eb5797decb641f5
leeyoonjie <ratte00@naver.com>
2023-08-14 23:07:32 +0900

    main/order/pick

---
+++ app/src/main/java/com/keyosk/kiosk/CafeMainActivity.kt
@@ -0,0 +1,12 @@
+package com.keyosk.kiosk
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CafeMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cafe_click)
+    }
+}
--- app/src/main/java/com/keyosk/kiosk/SecondActivity.kt
+++ app/src/main/java/com/keyosk/kiosk/SecondActivity.kt
@@ -1,11 +1,25 @@
 package com.keyosk.kiosk
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cafe_click)
+        setContentView(R.layout.activity_main)
+
+        val moveButton = findViewById<Button>(R.id.moveButton)
+
+        // 페이지 이동
+        fun moveToAnotherPage() {
+            val intent = Intent(this, CafeMainActivity::class.java)
+            startActivity(intent)
+        }
+        // 함수호출
+        moveButton.setOnClickListener {
+            moveToAnotherPage()
+        }
     }
 }
